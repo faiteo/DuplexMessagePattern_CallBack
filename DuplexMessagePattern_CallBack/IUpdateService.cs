bdfe428847cbc9734105dc5f746f1d0d287ffa61
Faith Nielsen <faiteo2002@yahoo.com>
2016-03-11 11:17:28 +0100

    WCF Service: Using Duplex Exchange Pattern with CallBack
    WCF Service: Using Duplex Exchange Pattern with CallBack

---
+++ DuplexMessagePattern_CallBack/IUpdateService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace DuplexMessagePattern_CallBack
+{
+    
+    [ServiceContract(CallbackContract = typeof(IUpdateServiceCallBack))]
+    public interface IUpdateService
+    {
+        [OperationContract(IsOneWay=true)]
+        void ProcessUpdate();
+    }
+
+    public interface IUpdateServiceCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void Update(int currentValue);
+    }
+}
---
+++ DuplexMessagePattern_CallBack/UpdateService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace DuplexMessagePattern_CallBack
+{
+    
+    public class UpdateService : IUpdateService
+    {
+        public void ProcessUpdate()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                OperationContext.Current.GetCallbackChannel<IUpdateServiceCallBack>().Update(i);
+                Thread.Sleep(5000);
+            }
+        }
+
+    }
+}
